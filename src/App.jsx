990dc2466d39b4ddde04b9a36f41cc5e784753a3
chaitu <srujandasari.2004@gmail.com>
2025-01-25 00:40:47 +0530

    Added JobPage
    covverted icons to FaMapMarker,FaAroowLeft

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { createBrowserRouter,createRoutesFromElements, Route, RouterProvider } f
 import MainLayout from './Layouts/MainLayout';
 import HomePage from './Pages/HomePage';
 import JobsPage from './Pages/JobsPage';
+import JobPage, {jobLoader}from './Pages/JobPage';
 import NotFoundPage from './Pages/NotFoundPage';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
   < Route index element = {<HomePage />}/>
   <Route path='jobs' element={<JobsPage/>} />
+  <Route path='jobs/:id' element={<JobPage/> } loader={jobLoader}  />
+
   <Route path='*' element={<NotFoundPage/>} />
 
     </Route>
